ec745ee95fd17bfdf87f92c323f744eb55d25b91
Joe Tavin <jtavin@gmail.com>
2021-06-28 22:36:21 +0300

    update models

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import logging, coloredlogs
 
 from utils import get_todays_date
 from database import db, migrate
-from models import Playlist, Song
+from models import Player, Playlist, Song
 
 app = Flask(__name__)
 coloredlogs.install(level='DEBUG', isatty=True)
@@ -15,30 +15,44 @@ db.init_app(app)
 migrate.init_app(app, db)
 db.create_all(app=app)
 
-def init_playlists():
+def init_player():
     appslify_api_endpoint = 'https://run.mocky.io/v3/4f6aee07-4831-4279-aecf-292fc05ec439'
     response = requests.get(appslify_api_endpoint)
-    for 
+    playlists = response.json()
+    logger.debug(f'Got from endpoint playlists: {playlists}')
+    for plist, songs in playlists.items():
+        logger.debug(f'Creating playlist with key [{plist}]')
+        pl = Playlist(plist)
+        for song in songs:
+            new_song = Song(song, pl.playlist_id)
+            logger.debug(f"Adding Song {song.song_name} to playlist")
+            pl.add_song(new_song)
+    
+    logger.info('Showing current playlist playing')
+
+    return(Player(get_todays_date()))
+
+player = init_player()
 
 @app.route('/api/v1/show_full_list')
 def show_full_list():
-    app.logger.info('Showing all playlists')
-
-    return(response.json())
+    return(logger.info('Showing all playlists'))
 
 @app.route('/api/v1/show_current_playlist')
 def show_current_playlist():
-    app.logger.info('Showing current playlist playing')
+    logger.info('Showing current playlist playing')
     playlist_id = get_todays_date()
     current_playlist = Playlist.query.get_or_404(playlist_id,
                            f'Sorry, couldn\'t find any Playlist with the id {playlist_id}')
     return(current_playlist)
 
 @app.route('/api/v1/play')
-def play_todays_playlist():
-    date = get_todays_date()
-    playlist = db.sess
-    return(f"Playlist with ID [{playlist.id}] and date [{playlist.playlist_date}] is currently playing")
+def play():
+    plist_id = get_todays_date()
+    logger.debug(f"Playlist with ID [{plist_id}] should be currently playing")
+    current_plist = Playlist.query.get_or_404(plist_id,
+                           f'Sorry, couldn\'t find any Playlist with the id {plist_id}')
+    logger.debug(f"Playlist includes songs {current_plist.songs}")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- app/models.py
+++ app/models.py
@@ -1,28 +1,43 @@
 """Application models."""
 
 from database import db
-from app import logger
+
+class Player(db.Model):
+    """Appslify music player."""
+
+    def __init__(self, playlist_id):
+        self.current_playlist = playlist_id
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    current_playlist = db.relationship('Playlist', backref='player', lazy=True)
 
 class Playlist(db.Model):
     """Song playlist."""
 
+    def __init__(self, playlist_id):
+        self.current_playlist = playlist_id
+        self.is_playing_now = False
+    
     playlist_id = db.Column(db.Integer, primary_key=True, nullable=False)
     is_playing_now = db.Column(db.Boolean, nullable=False)
-    songs = db.relationship('Song', backref='Playlist', lazy=True)
+    songs = db.relationship('Song', backref='playlist', lazy=True)
 
     def is_playing_now(self, is_playing_now):
         return self.is_playing_now
 
     def start_playing(self):
-        logger.debug(f"Playlist with ID [{self.playlist_id}] and date [{self.playlist_date}] is currently playing")
         self.is_playing_now == True
-
-    def get_playlist(self):
-        return self
+    
+    def add_song(self, song):
+        self.songs.append(song)
 
 class Song(db.Model):
     """Song"""
 
+    def __init__(self, name, playlist_id):
+        self.song_name = name
+        self.playlist_id = playlist_id
+    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     song_name = db.Column(db.String, nullable=False)
-    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'), nullable=False)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.playlist_id'), nullable=False)
